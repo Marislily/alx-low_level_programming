403fcdfb86561764058f848350519f97507b92ed
Marislily <chisomaris966@gmail.com>
2023-03-21 16:41:14 -0700

    98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -25,7 +25,7 @@ void print_to_98(int n)
 	}
 	else if (n < 0)
 	{
-		for (i = n ; i <= 98; i++)
+		for (i = n ; i <= 98 ; i++)
 		{
 			printf("%d", i);
 			if (i != 98)
@@ -37,7 +37,7 @@ void print_to_98(int n)
 	{
 		for (i = n ; i >= 98 ; i--)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
 			if (i != 98)
 				printf(", ");
 		}
