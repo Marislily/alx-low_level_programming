485e3ba1593cada0ac460f0f4c94b9443b73289d
Marislily <chisomaris966@gmail.com>
2023-03-23 19:34:42 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiplies
+ *
+ * @a: first number
+ *
+ * @b: second number
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+
+	return (0);
+}
