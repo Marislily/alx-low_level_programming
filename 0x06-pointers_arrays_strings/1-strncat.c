9ffc492d35f0244bdddb48d41768ead073b1ecd6
Marislily <chisomaris966@gmail.com>
2023-03-30 04:33:55 -0700

    1-strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - joins two strings
+ * @dest: copy to
+ * @src: copy from
+ * @n: number of char to be copied
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int s;
+	int t;
+
+	s = 0;
+	while (dest[s] != '\0')
+	{
+		s++;
+	}
+	t = 0;
+	while (t < n && src[t] != '\0')
+	{
+		dest[s] = src[t];
+		s++;
+		t++;
+	}
+	dest[s] = '\0';
+	return (dest);
+}
